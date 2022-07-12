013a101bbad98f02fa2b6a3e358d9420df4bb486
Murat Mujdeci <m.mujdeci@poshta.ua>
2022-07-12 04:43:47 +0300

    updat

--- lib/src/vpn_engine.dart
+++ lib/src/vpn_engine.dart
@@ -201,9 +201,9 @@ class OpenVPN {
 
   ///Request android permission (Return true if already granted)
   Future<bool> requestPermissionAndroid() async {
-    return await _channelControl
-        .invokeMethod("request_permission")
-        .then((value) => value ?? false);
+    final res = await _channelControl
+        .invokeMethod("request_permission");
+    return res ?? false;
   }
 
   ///Sometimes config script has too many Remotes, it cause ANR in several devices,
